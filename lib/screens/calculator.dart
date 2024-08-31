cc481fa8db60d3568c5bbadb0848979f3680fb6e
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2024-08-31 10:48:30 -0400

    componente Display

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:calculadora/components/display.dart';
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: [
+          Display('valores...'),
+          Text('Keyboard'),
+        ],
+      ),
+    );
+  }
+}
