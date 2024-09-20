224c75a0b196b6c5dc9c7f000ab72bf0361dc0fe
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2024-09-19 20:39:21 -0400

    correções dígito

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,9 +1,10 @@
 class Memory {
   static const operations = ['%', '/', 'x', '-', '+', '='];
-  String _value = '0';
+
   final _buffer = [0.0, 0.0];
-  String? _operation;
   int _bufferIndex = 0;
+  String? _operation;
+  String _value = '0';
   bool _wipeValue = false;
   String? _lastCommand;
 
@@ -12,6 +13,7 @@ class Memory {
       _operation = command;
       return;
     }
+
     if (command == 'AC') {
       _allClear();
     } else if (operations.contains(command)) {
@@ -31,9 +33,9 @@ class Memory {
   }
 
   _setOperation(String newOperation) {
-    bool isEqualSing = newOperation == '=';
+    bool isEqualSign = newOperation == '=';
     if (_bufferIndex == 0) {
-      if (!isEqualSing) {
+      if (!isEqualSign) {
         _operation = newOperation;
         _bufferIndex = 1;
         _wipeValue = true;
@@ -42,19 +44,36 @@ class Memory {
       _buffer[0] = _calculate();
       _buffer[1] = 0.0;
       _value = _buffer[0].toString();
-      _value = _value.endsWith('.') ? _value.split('.')[0] : _value;
-      _operation = isEqualSing ? null : newOperation;
-      _bufferIndex = isEqualSing ? 0 : 1;
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
     }
 
-    _wipeValue = true; //!isEqualSing;
+    _wipeValue = true; // !isEqualSign;
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if (isDot && _value.contains('.') && !wipeValue) {
+      return;
+    }
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   _allClear() {
     _value = '0';
     _buffer.setAll(0, [0.0, 0.0]);
-    _operation = null;
     _bufferIndex = 0;
+    _operation = null;
     _wipeValue = false;
   }
 
@@ -73,22 +92,6 @@ class Memory {
       default:
         return _buffer[0];
     }
-  }
-
-  _addDigit(String digit) {
-    final isDot = digit == '.';
-    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
-    final emptyValue = isDot ? '0' : '';
-    final currentValue = wipeValue ? emptyValue : _value;
-
-    if (isDot && _value.contains('.') && !wipeValue) {
-      return;
-    }
-
-    _value = currentValue + digit;
-    _wipeValue = false;
-
-    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   String get value {
